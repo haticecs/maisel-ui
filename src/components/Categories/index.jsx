688413fadff5f26a6ed79c458076b171a3de818d
haticecs <57989556+haticecs@users.noreply.github.com>
2021-10-25 23:27:08 +0300

    categories added

---
+++ src/components/Categories/index.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import CategoryItem from "./CategoriesItem/index";
+import { categories } from "../../data";
+
+const Container = styled.div`
+  display: flex;
+  padding: 20px;
+`;
+
+const index = () => {
+  return (
+    <Container>
+      {categories.map((item) => (
+        <CategoryItem item={item} />
+      ))}
+    </Container>
+  );
+};
+
+export default index;
