4ff5bdd26abfbedd9ba56f424cfa286f44c3b3f7
haticecs <57989556+haticecs@users.noreply.github.com>
2021-10-28 15:17:26 +0300

    file structure and imports

---
+++ src/components/index.jsx
@@ -0,0 +1,17 @@
+import Announcement from "./Announcement";
+import NavBar from "./NavBar";
+import Slider from "./Slider";
+import Categories from "./Categories";
+import Products from "./Products";
+import Newsletter from "./Newsletter";
+import Footer from "./Footer";
+
+export {
+  Announcement,
+  NavBar,
+  Slider,
+  Categories,
+  Products,
+  Newsletter,
+  Footer,
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import Announcement from "../components/Announcement";
-import NavBar from "../components/NavBar";
-import Slider from "../components/Slider";
-import Categories from "../components/Categories";
-import Products from "../components/Products";
-import Newsletter from "../components/Newsletter";
-import Footer from "../components/Footer";
+import {
+  Announcement,
+  NavBar,
+  Slider,
+  Categories,
+  Products,
+  Newsletter,
+  Footer,
+} from "../components";
 
 const Home = () => {
   return (
